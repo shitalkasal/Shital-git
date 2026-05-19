35ab77f6aa3b5a26b4ac7b9b389ea16079b9e5d0
ShitalKasal <shitalkasal9@gmail.com>
2026-05-19 10:23:54 +0200

    created file for subtraction

---
+++ sub.java
@@ -0,0 +1,8 @@
+public class sub {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int result = a - b;
+        System.out.println("The result of subtraction is: " + result);
+    }
+}
