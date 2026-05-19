6a1db3c0faea5d834387121c3e7c4bd1e088feb6
ShitalKasal <shitalkasal9@gmail.com>
2026-05-19 12:07:27 +0200

    updated information about the integers and variable

--- sum.java
+++ sum.java
@@ -10,3 +10,5 @@ public class Sum {
         System.out.println("The sum of " + a + " and " + b + " is: " + sum);
     }
 }
+# This program calculates the sum of two integers, `a` and `b`, and prints the result to the console.
+# The variables `a` and `b` are initialized with the values 5 and 10, respectively. The sum is calculated by adding `a` and `b`, and the result is stored in the variable `sum`. Finally, the program prints the sum to the console in a formatted string.
