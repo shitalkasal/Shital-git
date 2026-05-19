5ce7db6f35c5f3ec0779310f44471b2c10f8a2bd
ShitalKasal <shitalkasal9@gmail.com>
2026-05-19 10:20:37 +0200

    created division file

---
+++ div.java
@@ -0,0 +1,8 @@
+public class div {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        int result = a / b;
+        System.out.println("The result of " + a + " divided by " + b + " is: " + result);
+    }
+}
